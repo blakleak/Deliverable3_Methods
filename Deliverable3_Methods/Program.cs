8c99f7588fad5ee8c41f4412643aefc9cc2c6596
Amanda Blakley <51011635+blakleak@users.noreply.github.com>
2020-02-07 11:39:28 -0500

    This is version 2 of Deliverable 3 - Methods.

--- Deliverable3_Methods/Program.cs
+++ Deliverable3_Methods/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Name: Amanda Blakley
+ * Date: 2/6/2020
+ * Description: This C# console application code demonstrates
+ * the use of a method after getting input from a user. 
+ */
+
+using System;
 
 namespace Deliverable3_Methods
 {
@@ -6,7 +13,40 @@ namespace Deliverable3_Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Display a message welcoming the user and then
+            // prompt the user to enter their name
+            Console.WriteLine("Welcome! ");
+            Console.WriteLine();
+            Console.WriteLine("___________________________");
+            Console.WriteLine();
+            Console.WriteLine("Please enter your name....");
+
+            // Try-catch block handles possible errors 
+            try
+            {
+                // Stores user input for the application 
+                string input = Console.ReadLine();
+
+                // This is void method 
+                userName();
+                void userName()
+                {
+                    // Display the user's name and a message
+                    Console.WriteLine();
+                    Console.WriteLine("Hello " + input + "!");
+                    Console.WriteLine("___________________________");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to exit the application and try again...");
+                    Console.ReadKey(true);
+                }
+            }
+            catch
+            {
+                // Display an error message for the user 
+                Console.WriteLine("Please enter your name next time...");
+                Console.WriteLine("Press any key to exit the application and try again...");
+                Console.ReadKey(true);
+            } // End of catch 
         }
     }
 }
